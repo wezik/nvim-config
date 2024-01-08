d188d7305db28ac1d2d011e4d19c8264a63d6d66
Dominik <dominik.kondrot@gmail.com>
2024-01-08 14:32:18 +0100

    Italic variables fix
    Signed-off-by: Dominik <dominik.kondrot@gmail.com>

--- lua/wezik/plugins/treesitter.lua
+++ lua/wezik/plugins/treesitter.lua
@@ -21,6 +21,10 @@ return {
                 "markdown_inline",
             },
 
+            sync_install = false,
+            
+            auto_install = true,
+
             indent = {
                 enable = true,
             },
@@ -28,6 +32,7 @@ return {
             highlight = {
                 enable = true,
                 use_languagetree = true,
+                additional_vim_regex_highlighting = false,
             },
         })
     end,
