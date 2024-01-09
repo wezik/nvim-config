26554f34cc8150a32f1e31aa905611ae50dc99ce
Dominik <dominik.kondrot@gmail.com>
2024-01-09 05:43:52 +0100

    Diagnostic text & tildas removal

--- lua/wezik/set.lua
+++ lua/wezik/set.lua
@@ -1,5 +1,6 @@
 vim.opt.nu = true;
 vim.opt.relativenumber = true
+vim.opt.fillchars = "eob: "
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -29,4 +30,6 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
 vim.opt.completeopt = "menuone,noselect"
+
+vim.diagnostic.config ({ virtual_text = true })
 
