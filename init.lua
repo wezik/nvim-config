b7f4c15a30e2c00e86f0b55cb0d0b833af009e08
Dominik <dominik.kondrot@gmail.com>
2024-01-09 05:44:51 +0100

    LSP / CMP

--- init.lua
+++ init.lua
@@ -1,10 +1,33 @@
-require "wezik.bootstrap" 
+require "wezik.bootstrap"
 require "wezik.set"
 require "wezik.remap"
 
 require("lazy").setup("wezik.plugins")
 
 require "wezik.remap.plugins"
+
+local augroup = vim.api.nvim_create_augroup
+local Group = augroup("Group", {})
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd('LspAttach', {
+    group = Group,
+    callback = function(ev)
+        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+        -- keymaps for lsp
+        local opts = { buffer = ev.buf }
+    end
+})
+
+-- disable some default providers
+for _, provider in ipairs { "node", "perl", "python3", "ruby" } do
+    vim.g["loaded_" .. provider .. "_provider"] = 0
+end
+
+-- add binaries installed by mason.nvim to path
+local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
+vim.env.PATH = vim.fn.stdpath "data" .. "/mason/bin" .. (is_windows and ";" or ":") .. vim.env.PATH
+--
 
 -- require "core"
 --
